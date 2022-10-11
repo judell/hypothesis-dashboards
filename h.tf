e7bd037272d79f2799451333ce21f7ca58e5acdf
Jon Udell <judell@turbot.com>
2022-10-11 06:34:42 -0700

    add files

---
+++ h.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    steampipecloud = {
+      source = "turbot/steampipecloud"
+    }
+  }
+}
+
+resource "steampipecloud_workspace_mod" "hypothesis_dashboards" {
+  organization = "acme" 
+  workspace_handle = "jon"
+  path = "github.com/judell/hypothesis_dashboards"
+  constraint = "v0.1"
+}
